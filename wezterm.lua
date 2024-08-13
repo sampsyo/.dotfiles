a612c5ca48f88244b918d4365e88ea20cd3d8902
Adrian Sampson <adrian@radbox.org>
2024-08-13 07:44:18 -0400

    Fix Lua syntax

--- wezterm.lua
+++ wezterm.lua
@@ -2,19 +2,21 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 local act = wezterm.action
 
-config.color_scheme = 'Ayu Dark (Gogh)'
-config.window_decorations = 'RESIZE|MACOS_FORCE_ENABLE_SHADOW'
+config.color_scheme = 'Ir Black (Gogh)'
+config.window_decorations = 'RESIZE|MACOS_FORCE_ENABLE_SHADOW' ..
+  '|INTEGRATED_BUTTONS'
 config.quit_when_all_windows_are_closed = false
 
 config.font = wezterm.font_with_fallback {
-  'Source Code Pro',
-  'SourceCodePro',
+  { family = 'Cascadia Mono', weight = 'Medium' },
+  { family = 'Source Code Pro', weight = 'Medium' },
+  { family = 'SourceCodePro', weight = 'Medium' },
   'Menlo',
 }
-config.font_size = 13.0
+config.font_size = 12.0
 
 config.native_macos_fullscreen_mode = true
-config.window_background_opacity = 0.85
+config.window_background_opacity = 0.87
 config.macos_window_background_blur = 40
 
 config.keys = {
