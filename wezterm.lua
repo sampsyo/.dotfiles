a29ec1f5283f5ab228505039ecb5efe640471112
Adrian Sampson <adrian@radbox.org>
2024-08-13 06:32:44 -0400

    More WezTerm

--- wezterm.lua
+++ wezterm.lua
@@ -2,9 +2,20 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 local act = wezterm.action
 
-config.color_scheme = 'AdventureTime'
-config.window_decorations = 'RESIZE'
+config.color_scheme = 'Ayu Dark (Gogh)'
+config.window_decorations = 'RESIZE|MACOS_FORCE_ENABLE_SHADOW'
 config.quit_when_all_windows_are_closed = false
+
+config.font = wezterm.font_with_fallback {
+  'Source Code Pro',
+  'SourceCodePro',
+  'Menlo',
+}
+config.font_size = 13.0
+
+config.native_macos_fullscreen_mode = true
+config.window_background_opacity = 0.85
+config.macos_window_background_blur = 40
 
 config.keys = {
   {
